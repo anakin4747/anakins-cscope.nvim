cc0de8f595f710f595c2a27bc503a823e4de68ca
Anakin Childerhose <anakin@childerhose.ca>
2026-06-14 18:47:13 -0400

    feat: add make dev target for testing it out
    jhkjhjk

--- Makefile
+++ Makefile
@@ -5,6 +5,12 @@ cqfd:
 	@./scripts/cqfd/cqfd init > /dev/null
 	@./scripts/cqfd/cqfd run make tests
 
+.PHONY: dev
+dev:
+	@git submodule update --init > /dev/null
+	@./scripts/cqfd/cqfd init > /dev/null
+	@./scripts/cqfd/cqfd run nvim -u tests/dev/init.lua tests/fixtures/default/arch/arm/kernel/setup.c
+
 .PHONY: test tests
 test tests:
 	@cog check --from-latest-tag
---
+++ tests/dev/init.lua
@@ -0,0 +1,21 @@
+vim.opt.runtimepath:append('.')
+vim.opt.swapfile = false
+local ac = require('anakins-cscope')
+
+vim.cmd('cd ./tests/fixtures/default/')
+
+ac.should_log = true
+ac.logfile = 'anakins-cscope.log'
+
+vim.g.mapleader = ' '
+
+vim.keymap.set({ 'n', 'v' }, '<leader>ic', ac.goto_incoming_calls)
+vim.keymap.set({ 'n', 'v' }, '<leader>oc', ac.goto_outgoing_calls)
+vim.keymap.set({ 'n', 'v' }, 'gd', ac.goto_definition)
+vim.keymap.set({ 'n', 'v' }, 'grn', ac.rename)
+
+vim.keymap.set('n', '<leader>m', function()
+    vim.cmd('redir! > ../../dev/messages.log')
+    vim.cmd('messages')
+    vim.cmd('redir END')
+end)
